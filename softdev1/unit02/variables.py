6a555099934c57e1ea93078085e4050c7c6eca9e
Tre Reese <treese458@gmail.com>
2022-08-31 13:23:19 -0400

    4th comm

---
+++ softdev1/unit02/variables.py
@@ -0,0 +1,9 @@
+def variable_practice():
+    age =18 *12
+    days_in_year = 365
+    first_pet = "kolby"
+    pi_digits = 3.14159
+
+    print("Age:" , age, "\n" "Days in year: ", days_in_year)
+
+variable_practice()
