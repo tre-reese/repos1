b58c82b85bd08cb60b570b17663b600203564e50
Tre Reese <treese458@gmail.com>
2022-08-31 12:41:18 -0400

    intial commit

---
+++ happy_bday.py
@@ -0,0 +1,7 @@
+
+def bday_message()
+    name = input("What is your name")
+    month = input("What is your birth month")
+    day = input("what is your birth day")
+    year =input("What is your birth year")
+    print(day,month ,year)
