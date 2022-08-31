923a82371fa32261ded1a41766b2e2f3206926ab
Tre Reese <treese458@gmail.com>
2022-08-31 12:51:44 -0400

    secondary comm

--- happy_bday.py
+++ happy_bday.py
@@ -1,7 +1,11 @@
 
-def bday_message()
+def bday_message():
     name = input("What is your name")
     month = input("What is your birth month")
     day = input("what is your birth day")
     year =input("What is your birth year")
-    print(day,month ,year)
+    print("Your birhtday is on" + day,month,year)
+
+bday_message()
+bday_message()
+bday_message()
