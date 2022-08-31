3975bafeedb72337d2d058057a367f1ae00c1ad1
Tre Reese <treese458@gmail.com>
2022-08-31 13:35:45 -0400

    5th comm

--- softdev1/unit02/variables.py
+++ softdev1/unit02/variables.py
@@ -7,3 +7,12 @@ def variable_practice():
     print("Age:" , age, "\n" "Days in year: ", days_in_year)
 
 variable_practice()
+def expressions_practice():
+    literal = 24
+    addition = 24 + 45
+    exponent = 12**2
+    floor_div = 92//10
+    mod = 6%2
+    mix = (4+5-2)*3
+    print( literal, addition, exponent, floor_div, mod, mix)
+expressions_practice()
