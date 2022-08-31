cd47d7e00578af676b0a770b3861ae9d89fc4be1
Tre Reese <treese458@gmail.com>
2022-08-31 13:06:59 -0400

    tertiary comm

---
+++ softdev1/unit02/hello.py
@@ -0,0 +1,17 @@
+#This program gives the user a specialized hello
+#Tre Reese
+
+def hello():
+    #defines the function
+    print("Hello world!")
+    #prints hello world
+def hello2(): 
+    #defines the other function
+    name = input("What is your name")
+    #allows user name input
+    print ("Hello," + name)
+    #prints hello and the users name
+    
+hello()
+hello2()
+#^^calls functions
