499c71abb527a4af4fb62827808e22255adc8bfd
Tre Reese <treese458@gmail.com>
2022-09-07 12:59:00 -0400

    secondcommit

--- softdev1/unit03/practice.py
+++ softdev1/unit03/practice.py
@@ -1,8 +1,39 @@
-def convert_height():
-   height = int(input("Enter height in inches: "))
-   height_2 = height//12
-   height_3 = height%12
 
-   print("You are ", height_2,  height_3, "", "tall")  
-convert_height()
 
+import turtle
+def main():
+    turtle.penup()
+    turtle.goto(-400,-300)
+    turtle.pencolor("blue")
+    turtle.fillcolor("blue")
+    turtle.begin_fill()
+    turtle.pendown()
+    turtle.goto(400,-300)
+    turtle.goto(400,400)
+    turtle.goto(-400,400)
+    turtle.goto(-400,-300)
+    turtle.end_fill()
+    turtle.penup()
+    def snow_man():
+        turtle.fillcolor("white")
+        turtle.pencolor("black")
+        turtle.goto(0,0)
+        turtle.begin_fill()
+        turtle.pendown()
+        turtle.circle(50)
+        turtle.end_fill()
+        turtle.penup()
+        turtle.goto(0,100)
+        turtle.begin_fill()
+        turtle.pendown()
+        turtle.circle(50*.75)
+        turtle.end_fill()
+        turtle.penup()
+        turtle.goto(0,175)
+        turtle.begin_fill()
+        turtle.pendown()
+        turtle.circle(50*.56)
+        turtle.end_fill()
+    snow_man()
+main()
+input()
