290f6e131108f08c90fd7e3fa2b1ae937439ead7
Tre Reese <treese458@gmail.com>
2022-09-14 09:22:12 -0400

    test commit

---
+++ softdev1/unit03/guessing_test.py
@@ -0,0 +1,11 @@
+from asyncio.windows_utils import pipe
+from pickletools import pybytes_or_str
+from random import randrange
+#setup
+inp1 = 0
+def test_check_guess():
+    n = randrange(1,10)
+    if inp1 <1:
+        return("Guess not in range")
+def test_check_guess_less_than_1():
+    test_check_guess()
