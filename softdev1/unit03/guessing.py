39bd9becb6d618dff2623d400c1b495cf99dfe03
Tre Reese <treese458@gmail.com>
2022-09-14 10:13:42 -0400

    final commit

--- softdev1/unit03/guessing.py
+++ softdev1/unit03/guessing.py
@@ -1,7 +1,37 @@
 from asyncio.windows_utils import pipe
 from pickletools import pybytes_or_str
 from random import randrange
-python -m pip install -U pytest
+num = randrange(1,10,1)
+def check_guess():
+   inp1 = int(input("Please enter a number 1-10 "))
+   if inp1 <1 or inp1 >10:
+        resp1 = "Guess not in range!"
+        print(resp1)
+        return resp1
+  
+        
+   if inp1 > num:
+        resp2 ="Too high!"
+        print(resp2)
+        return resp2
+        
+   if inp1 < num:
+        resp3 = "Too low!"
+        print(resp3)
+        return resp3
+       
+   if inp1 == num:
+        resp4 = "Correct!"
+        print(resp4)
+        return  resp4
+    
+def main():
+    check_guess()
+    check_guess()
+    check_guess()
+main()
+
+    
 
 
 
