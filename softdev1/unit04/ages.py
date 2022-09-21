e29c4c0c4f848bfe2daae4df3be3bcb776ae80e3
Tre Reese <treese458@gmail.com>
2022-09-21 13:18:08 -0400

    full commit

---
+++ softdev1/unit04/ages.py
@@ -0,0 +1,11 @@
+def human_years():
+    dog_year = int(input("Enter a dog age:"))
+    age = dog_year /7
+    print("The dog would be", age , "years in human age")
+human_years()
+def dog_years():
+    human_year = int(input("enter an age: "))
+    age2 = human_year//7
+    print("The person would be", age2 , "years in dog age")
+dog_years()
+
